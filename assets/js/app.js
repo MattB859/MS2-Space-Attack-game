fda00546fd183ce3e86cf5ccc75f0fd58f18b53e
MattB859 <mateus756@hotmail.com>
2021-08-13 17:50:29 +0000

    Add img to background and styling

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,18 @@
+let myGamePiece;
+let myObstacles = [];
+let myScore;
+
+
+
 function startGame(){
-    
+
+}
+
+let myGameArea = {
+    canvas: document.createElement("canvas"),
+    start: function() {
+        this.canvas.width = 480;
+        this.canvas.height = 270;
+        this.context = this.canvas.getContext("2d");
+    }
 }
