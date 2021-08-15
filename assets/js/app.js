0b0d174c7780a17431371d5c0d3f3f258f64ef3c
MattB859 <mateus756@hotmail.com>
2021-08-15 09:41:13 +0000

    Add three variables to app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,4 @@
 let myGamePiece;
-let myObstacles = [];
-let myScore;
+let myObstacles;
+let MyScore;
 
-
-
-function startGame(){
-
-}
-
-let myGameArea = {
-    canvas: document.createElement("canvas"),
-    start: function() {
-        this.canvas.width = 480;
-        this.canvas.height = 270;
-        this.context = this.canvas.getContext("2d");
-    }
-}
