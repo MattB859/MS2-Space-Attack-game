c97fc65ef9d27f35c355caf1ff3a0a3d76dfb77b
MattB859 <mateus756@hotmail.com>
2021-08-15 10:01:53 +0000

    Add function called myGame to app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,9 +2,19 @@ let myGamePiece;
 let myObstacles;
 let MyScore;
 
-const background = new Image();
+function myGame() {
+    myGamePiece = new component(30, 30, "red", 10, 120);
+    myGamePiece.gravity = 0.05;
+    MyScore = new component("30px", "consolas", "black", 280, 40, "text");
+    myGameArea.start();
+}
+
+
+
+
+let background = new Image();
 background.src = 'sea_background.png';
-const bg = {
+let bg = {
     x1: 0,
     x2: canvas.width,
     y: 0,
