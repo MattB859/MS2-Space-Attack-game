5753dfc3bfa52098694fc26c92f1c3da616b9f51
MattB859 <mateus756@hotmail.com>
2021-08-15 11:10:50 +0000

    Add new function for game component

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,38 +1,31 @@
-let myGamePiece;
-let myObstacles;
-let MyScore;
 
-function myGame() {
+let myGamePiece;
+let myObstacles = [];
+let myScore;
+
+
+function startGame() {
     myGamePiece = new component(30, 30, "red", 10, 120);
     myGamePiece.gravity = 0.05;
-    MyScore = new component("30px", "consolas", "black", 280, 40, "text");
+    myScore = new component("30px", "consolas", "black", 280, 40, "text");
     myGameArea.start();
 }
 
 let myGameArea = {
     canvas : document.createElement("canvas"),
-    start : function(){
+    start : function() {
         this.canvas.width = 480;
         this.canvas.height = 270;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.frameNo = 0;
-        this.interval = setInterval(updateGameArea, 20);
+        this.imterval = setInterval(updateGameArea, 20);
     },
-    clear : function() {
-        this.context.clearReact(0, 0, this.canvas.width, this.canvas.height);
+    clear : function(){
+        this.context.clearReact(0, 0, this.canvas.width, this.canvas.height)
     }
 }
 
-
-
-
-let background = new Image();
-background.src = 'sea_background.png';
-let bg = {
-    x1: 0,
-    x2: canvas.width,
-    y: 0,
-    width: canvas.width,
-    height: canvas.height
+function component(width, height, color, x, y, type){
+    
 }
