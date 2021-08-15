2f54557cfc1c530aa64f8a17921906a19fef9279
MattB859 <mateus756@hotmail.com>
2021-08-15 10:14:59 +0000

    Add object called my game area

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,6 +9,21 @@ function myGame() {
     myGameArea.start();
 }
 
+let myGameArea = {
+    canvas : document.createElement("canvas"),
+    start : function(){
+        this.canvas.width = 480;
+        this.canvas.height = 270;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.frameNo = 0;
+        this.interval = setInterval(updateGameArea, 20);
+    },
+    clear : function() {
+        this.context.clearReact(0, 0, this.canvas.width, this.canvas.height);
+    }
+}
+
 
 
 
