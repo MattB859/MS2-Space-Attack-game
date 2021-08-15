0f16487609892e3592871404f89c9dceff599590
MattB859 <mateus756@hotmail.com>
2021-08-15 09:52:11 +0000

    Add a variable and object to place background image on canvas

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,3 +2,12 @@ let myGamePiece;
 let myObstacles;
 let MyScore;
 
+const background = new Image();
+background.src = 'sea_background.png';
+const bg = {
+    x1: 0,
+    x2: canvas.width,
+    y: 0,
+    width: canvas.width,
+    height: canvas.height
+}
