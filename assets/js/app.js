b26c367b1ddf43a08f09d0a30c0cd04f9be49061
MattB859 <mateus756@hotmail.com>
2021-08-16 10:59:55 +0000

    Add 3 new variables to control game score, speed and angle

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,14 @@
-let canvas = document.getElementById("canvas");
-let ctx = canvas.getContext("2d")
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+canvas.width = 600;
+canvas.height = 400;
+
+let spacePressed = false;
+let angle = 0;
+let hue = 0;
+let frame = 0;
+let score = 0;
+let gamespeed = 2;
+
+
+
