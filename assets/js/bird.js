6826266dec9d6c165759f3abea5f825ade459dd2
MattB859 <mateus756@hotmail.com>
2021-08-17 17:08:06 +0000

    Bug conditional statement not working

--- assets/js/bird.js
+++ assets/js/bird.js
@@ -1,4 +1,5 @@
 class Bird {
+
     constructor(){
         this.x = 150;
         this.y = 200;
@@ -8,10 +9,21 @@ class Bird {
         this.weight = 1;
     }
     update(){
-        this.vy += this.weight;
-        this.y += this.vy;
+        if (this.y > canvas.height - (this.height * 3)){
+            this.y = canvas.height - (this.height * 3);
+            this.vy = 0;
+        } else {
+            this.vy += this.weight;
+            this.y += this.vy;
+        } if (spacePressed) this.flap();
+       
     }
     draw(){
-        ctx.fillstyle
+        ctx.fillStyle = 'red';
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+    flap(){
+        this.vy -= 2;
     }
 }
+const bird = new Bird();
