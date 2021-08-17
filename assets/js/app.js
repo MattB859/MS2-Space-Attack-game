ff46149b19dee8d4fda4ebad0043974a83f97525
MattB859 <mateus756@hotmail.com>
2021-08-17 18:17:59 +0000

    Add new variable

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,10 @@
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
 canvas.width = 600;
 canvas.height = 400;
 
 let spacePressed = false;
+let buttonPressed = false;
 let angle = 0;
 let hue = 0;
 let frame = 0;
@@ -13,29 +14,29 @@ let gamespeed = 2;
 let temp = canvas.height - 90;
 
 // Custom code by developer
-function animate(){
+ animate = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillRect(10, canvas.height - 90, 50, 50);
+    //ctx.fillRect(10, canvas.height - 90, 50, 50);
+    bird.update();
+    bird.draw();
     requestAnimationFrame(animate);
 }
 animate()
 
-window.addEventListener('keydown', function(e) {
+window.addEventListener('keydown', (e) => {
  if (e.code === 'Space') spacePressed = true;
 
-
 });
-window.addEventListener('keydown', function(e) {
+window.addEventListener('keydown', (e) => {
     if (e.code === 'Space') spacePressed = false;
    });
    
-// Custom code by developer
-window.addEventListener('mousedown', function(e) {
-    if (e.code === 'Click') spacePressed = true;
+
+window.addEventListener('mousedown', (e) => {
+    if (e.code === 'Click') buttonPressed = true;
 
 });
 
-// Custom code by developer
-window.addEventListener('mouseup', function(e) {
-       if (e.code === 'Click') spacePressed = false;
+window.addEventListener('mouseup', (e) => {
+       if (e.code === 'Click') buttonPressed = false;
 });
