1f998aaa226ef98b6b9e3d754390ca66ccca0deb
MattB859 <mateus756@hotmail.com>
2021-08-18 14:28:16 +0000

    Add empty array and a class

---
+++ assets/js/particles.js
@@ -0,0 +1,21 @@
+let particlesArray = [];
+
+class Particle {
+    constructor(){
+        this.x = bird.x;
+        this.y = bird.y;
+        this.size = Math.random() * 7 + 3;
+        this.speedY = (Math.random() * 1) - 0.5;
+        this.color = 'red';
+    }
+    update(){
+        this.x -= gamespeed;
+        this.y += this.speedY;
+    }
+    draw(){
+        ctx.fillStyle = this.color;
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        ctx.fill();
+    }
+}
