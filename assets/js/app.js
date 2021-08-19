0e5b0a8f6cbde65ab33660ad4fe61063f31218c6
MattB859 <mateus756@hotmail.com>
2021-08-19 15:24:33 +0000

    Add beautify to all files

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,7 +4,6 @@ canvas.width = 600;
 canvas.height = 400;
 
 let spacePressed = false;
-let click = false;
 let angle = 0;
 let hue = 0;
 let frame = 0;
@@ -12,24 +11,24 @@ let score = 0;
 let gamespeed = 2;
 
 // Custom code by developer 
- animate = () => {
+animate = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    handleObstacles();
     bird.update();
     bird.draw();
     handleParticles();
     requestAnimationFrame(animate);
-    angle+=0.12;
+    angle += 0.12;
     hue++;
+    frame++;
 }
 animate();
 
 // Custom code by developer
 window.addEventListener('keydown', (e) => {
- if (e.code === 'Space') spacePressed = true;
+    if (e.code === 'Space') spacePressed = true;
 
 });
-window.addEventListener('keyup', (e) =>  {
+window.addEventListener('keyup', (e) => {
     if (e.code === 'Space') spacePressed = false;
-   });
-   
-   
+});
--- assets/js/bird.js
+++ assets/js/bird.js
@@ -1,5 +1,5 @@
 class Bird {
-    constructor(){
+    constructor() {
         this.x = 150;
         this.y = 200;
         this.vy = 0;
@@ -7,27 +7,27 @@ class Bird {
         this.height = 20;
         this.weight = 1;
     }
-    update(){
+    update() {
         let curve = Math.sin(angle) * 20;
-        if (this.y > canvas.height - (this.height * 3) + curve){
+        if (this.y > canvas.height - (this.height * 3) + curve) {
             this.y = canvas.height - (this.height * 3) + curve;
             this.vy = 0;
         } else {
             this.vy += this.weight;
             this.vy *= 0.9;
             this.y += this.vy;
-        } 
-        if (this.y < 0 + this.height){
+        }
+        if (this.y < 0 + this.height) {
             this.y = 0 + this.height;
             this.vy = 0;
         }
-        if (spacePressed && this.y > this.height * 3) this.flap();      
+        if (spacePressed && this.y > this.height * 3) this.flap();
     }
-    draw(){
+    draw() {
         ctx.fillStyle = 'red';
         ctx.fillRect(this.x, this.y, this.width, this.height);
     }
-    flap(){
+    flap() {
         this.vy -= 2;
     }
 }
