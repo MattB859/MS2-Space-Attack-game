5321ffbbc2e5bb584733e86a9ff8e892e47aa2d7
MattB859 <mateus756@hotmail.com>
2021-08-21 16:14:03 +0000

    Add collision function to app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,27 +1,25 @@
+// Custom code by developer 
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext('2d');
 canvas.width = 600;
 canvas.height = 400;
 
-// Custom code by developer 
 let spacePressed = false;
 let angle = 0;
 let hue = 0;
 let frame = 0;
 let score = 0;
 let gamespeed = 2;
-let mySound;
-let myMusic;
+let gameMusic; 
 
-startGme = () => {
-   myMusic = new sound('/sound/jazz-smasher.mp3');
   
-}
+
 
 // Custom code by developer 
 animate = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     handleObstacles();
+   
     bird.update();
     bird.draw();
     ctx.fillStyle = 'red';
@@ -35,7 +33,7 @@ animate = () => {
     angle += 0.12;
     hue++;
     frame++;
-
+   
 }
 animate();
 
@@ -48,27 +46,25 @@ window.addEventListener('keyup', (e) => {
     if (e.code === 'Space') spacePressed = false;
 });
 
+// custom code by developer for sound fx
 class sound {
     constructor(src) {
         this.sound = document.createElement("audio");
         this.sound.src = src;
-        this.sound.setAttribute("preload", "auto");
-        this.sound.setAttribute("controls", "none");
-        this.sound.style.display = "none";
         document.body.appendChild(this.sound);
-        this.play = function () {
+        this.play = () => {
             this.sound.play();
         };
-        this.stop = function () {
+        this.stop = () => {
             this.sound.pause();
         };
     }
 }
 
-// Code taken from franks laboratory.
 const bang = new Image();
 bang.src = '/assets/images/explosion.png';
-let fxSound = new sound('/sound/explosion.mp3');
+let fxSound = new sound('sound/explosion.mp3');
+gameMusic = new sound('/sound/jazz-smasher.mp3');
 
 function handleCollisions() {
     for (let i = 0; i < obstaclesArray.length; i++) {
@@ -78,11 +74,11 @@ function handleCollisions() {
                 (bird.y > canvas.height - obstaclesArray[i].bottom &&
                     bird.y + bird.height < canvas.height))) {
             // collision detected
-            ctx.drawImage(bang, bird.x, bird.y, 50, 50); 
             fxSound.play(); 
+            ctx.drawImage(bang, bird.x, bird.y, 50, 50); 
             ctx.font = "25px Georgia";
             ctx.fillStyle = 'black';
-            ctx.fillText('Game Over, your score is' + score, 160, canvas.height/2 )
+            ctx.fillText('Game Over, your score is ' + score, 160, canvas.height/2)
             return true;
 
         }
